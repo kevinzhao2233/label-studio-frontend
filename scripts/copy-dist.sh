0ab26bf4cd3fdebf5c1354704c8e56703eb860ed
赵凯文 <kevin_2233@qq.com>
2022-02-14 14:42:53 +0800

    🔧 copy-dist 加入打印信息

--- scripts/copy-dist.sh
+++ scripts/copy-dist.sh
@@ -3,3 +3,5 @@ cp -f build/main.js.map ../label-studio/label_studio/frontend/dist/lsf/js
 
 cp -f build/main.css.map ../label-studio/label_studio/frontend/dist/lsf/css
 cp -f build/main.css.map ../label-studio/label_studio/frontend/dist/lsf/css
+
+echo '构建成果已复制到 label-studio'
